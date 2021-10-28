f4c20f2aad3c78fe46e9174fac026abe7c609c1a
MauricioDias <mauriciodyas@gmail.com>
2021-10-28 12:09:28 +1100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mauricio Dias on 28/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
